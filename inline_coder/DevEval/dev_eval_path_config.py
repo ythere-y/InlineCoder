5d326b252e59862d28c495158b0e9ea29eadd692
ythere <1069043992@qq.com>
2025-09-11 16:44:58 +0800

    distribute

--- inline_coder/DevEval/dev_eval_path_config.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-class PathConfig:
-    BenchmarkDir = Path("data/Benchmark/dev_eval")
-    OriginDataPath = BenchmarkDir / "processed_clean_data" / "data.jsonl"
--- inline_coder/Models/name_statics.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class ModelName(Enum):
-    DeepSeekV3 = "DeepSeekV3"
-    Qwen3_Coder = "Qwen3_Coder"
-    GPT5 = "gpt-5-mini"
